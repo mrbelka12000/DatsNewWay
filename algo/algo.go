457b4c0a81ac9977119f9608fa8105a90300e67e
beknur.karshyga <beknur.karshyga@armenotech.com>
2024-12-20 22:50:49 +0500

    bfs added

--- algo/algo.go
+++ algo/algo.go
@@ -1,7 +1,7 @@
 package algo
 
 import (
-	"math"
+	"fmt"
 
 	"DatsNewWay/entity"
 )
@@ -11,37 +11,146 @@ const (
 	snakeStatusDead  = "dead"
 )
 
+type node struct {
+	point []int
+	next  *node
+}
+
 func GetNextDirection(r entity.Response) (obj entity.Payload) {
-	used := make(map[int]bool)
+	return bfs(r)
+	//used := make(map[int]bool)
+	//
+	//for _, snake := range r.Snakes {
+	//	if snake.Status == snakeStatusDead {
+	//		continue
+	//	}
+	//	var (
+	//		direction []int
+	//		minDist   = math.MaxInt32
+	//		minInd    int
+	//	)
+	//
+	//	for i, food := range r.Food {
+	//		dist := getManhattanDistance(snake.Geometry[0], food.C)
+	//		if dist < minDist && !used[i] {
+	//			minDist = dist
+	//			minInd = i
+	//			direction = getDirection(snake.Geometry[0], food.C)
+	//		}
+	//	}
+	//
+	//	used[minInd] = true
+	//
+	//	obj.Snakes = append(obj.Snakes, entity.Snake{
+	//		Id:        snake.Id,
+	//		Direction: direction,
+	//	})
+	//}
+
+	//return obj
+}
+
+func bfs(r entity.Response) (obj entity.Payload) {
+
+	obst := make(map[[3]int]bool)
+
+	// fill obstacles with fences
+	for _, fence := range r.Fences {
+		key := [3]int{fence[0], fence[1], fence[2]}
+		obst[key] = true
+	}
+
+	// fill obstacles with enemies
+	for _, enemy := range r.Enemies {
+		for _, coord := range enemy.Geometry {
+			key := [3]int{coord[0], coord[1], coord[2]}
+			obst[key] = true
+		}
+	}
+
+	food := make(map[[3]int]bool)
+	// fill food hash table
+	for _, f := range r.Food {
+		key := [3]int{f.C[0], f.C[1], f.C[2]}
+		food[key] = true
+	}
 
 	for _, snake := range r.Snakes {
-		if snake.Status == snakeStatusDead {
-			continue
+		if snake.Status == snakeStatusAlive {
+			dir := runner(r, snake.Geometry[0], obst, food)
+			obj.Snakes = append(obj.Snakes, entity.Snake{
+				Id:        snake.Id,
+				Direction: dir,
+			})
 		}
-		var (
-			direction []int
-			minDist   = math.MaxInt32
-			minInd    int
-		)
-
-		for i, food := range r.Food {
-			dist := getManhattanDistance(snake.Geometry[0], food.C)
-			if dist < minDist && !used[i] {
-				minDist = dist
-				minInd = i
-				direction = getDirection(snake.Geometry[0], food.C)
-			}
-		}
-
-		used[minInd] = true
-
-		obj.Snakes = append(obj.Snakes, entity.Snake{
-			Id:        snake.Id,
-			Direction: direction,
-		})
 	}
 
 	return obj
+}
+
+func runner(r entity.Response, currPoint []int, obst, food map[[3]int]bool) []int {
+	dirs := [6][]int{
+		{1, 0, 0},
+		{-1, 0, 0},
+		{0, 1, 0},
+		{0, -1, 0},
+		{0, 0, 1},
+		{0, 0, -1},
+	}
+	type info struct {
+		point []int
+		steps []int
+		cost  int
+	}
+
+	step := make(map[[3]int]info)
+
+	q := []info{
+		{
+			point: currPoint,
+		},
+	}
+
+	for len(q) > 0 {
+
+		curr := q[0]
+		q = q[1:]
+
+		cp := curr.point
+
+		if food[[3]int{cp[0], cp[1], cp[2]}] {
+			fmt.Println(curr.steps, cp[0], cp[1], cp[2])
+			return curr.steps
+		}
+
+		for _, dir := range dirs {
+			xx, yy, zz := cp[0]+dir[0], cp[1]+dir[1], cp[2]+dir[2]
+
+			if xx < 0 || xx >= r.MapSize[0] || yy < 0 || yy >= r.MapSize[1] || zz < 0 || zz >= r.MapSize[2] {
+				continue
+			}
+
+			if obst[[3]int{xx, yy, zz}] {
+				continue
+			}
+
+			val, ok := step[[3]int{xx, yy, zz}]
+			if !ok || val.cost > curr.cost {
+				steps := dir
+				if len(curr.steps) != 0 {
+					steps = curr.steps
+				}
+
+				step[[3]int{xx, yy, zz}] = info{
+					point: []int{xx, yy, zz},
+					steps: steps,
+					cost:  curr.cost + 1,
+				}
+			}
+		}
+	}
+
+	return nil
 }
 
 func getDirection(head, target []int) []int {
